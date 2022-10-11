8e919869cfd6dd65b852e131b151cc38bef3eef8
Billy Fowks <fowkswe@gmail.com>
2022-10-10 22:10:20 -0500

    7ef51e1

---
+++ _nuxt/static/1665457817/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftheintangibles.github.io\u002F_nuxt\u002Fstatic\u002F1665457817",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Ftheintangibles.github.io\u002F",assetsPath:"\u002Ftheintangibles.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
